52f6dd2ba2ffc856cb15b89b63c067f66d3bb970
claudiocarmusciano <claudiocarmusciano@gmail.com>
2023-09-23 20:24:13 -0300

    se creó archivo de Factory y main vacío

---
+++ src/main/java/com/example/tpintegrador2/Factory/EntityFactory.java
@@ -0,0 +1,34 @@
+package com.example.tpintegrador2.Factory;
+
+import javax.persistence.Persistence;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+
+
+
+public class EntityFactory {
+
+        private final EntityManagerFactory emf;
+
+        private EntityFactory() {
+            emf = Persistence.createEntityManagerFactory("integrador2");
+        }
+
+        private static class SingletonHelper {
+            private static final EntityFactory INSTANCE = new EntityFactory();
+        }
+
+        public static EntityFactory getInstance() {
+            return SingletonHelper.INSTANCE;
+        }
+
+        public EntityManager createEntityManager() {
+            return emf.createEntityManager();
+        }
+
+        public void closeEntityManagerFactory() {
+            if (emf != null && emf.isOpen()) {
+                emf.close();
+            }
+        }
+}
---
+++ src/main/java/com/example/tpintegrador2/Main.java
@@ -0,0 +1,15 @@
+package com.example.tpintegrador2;
+
+import com.example.tpintegrador2.Factory.EntityFactory;
+import com.example.tpintegrador2.Entidades.Estudiante;
+import com.example.tpintegrador2.Entidades.Carrera;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        
+    }
+}
